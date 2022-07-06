98bfa8aacfadcd7f34534cf2e47611801204091b
ijstartcanonsetip <108786619+ijstartcanonsetip@users.noreply.github.com>
2022-07-06 12:23:52 +0530

    Update index.php
    Signed-off-by: ijstartcanonsetip <108786619+ijstartcanonsetip@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,154 +1,109 @@
-
 <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-      <meta charset="UTF-8">
-      <title>Cricut Download & Install</title>
-      <link rel='stylesheet' id='astra-theme-css-css'  href='wp-content/themes/astra/assets/css/minified/main.mina4f3a4f3.css?ver=3.6.8' media='all' />
-      <link rel='stylesheet' id='wp-block-library-css'  href='wp-includes/css/dist/block-library/style.minf658f658.css?ver=5.8.1' media='all' />
-      <link rel='stylesheet' id='elementor-icons-css'  href='wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.minb683b683.css?ver=5.12.0' media='all' />
-      <link rel='stylesheet' id='e-animations-css'  href='wp-content/plugins/elementor/assets/lib/animations/animations.minccfbccfb.css?ver=3.4.2' media='all' />
-      <link rel='stylesheet' id='elementor-frontend-css'  href='wp-content/plugins/elementor/assets/css/frontend.minccfbccfb.css?ver=3.4.2' media='all' />
-      <link rel='stylesheet' id='elementor-post-5-css'  href='wp-content/uploads/elementor/css/post-585d985d9.css?ver=1630066158' media='all' />
-      <link rel='stylesheet' id='elementor-global-css'  href='wp-content/uploads/elementor/css/globala5b9a5b9.css?ver=1630066166' media='all' />
-      <link rel='stylesheet' id='elementor-post-12-css'  href='wp-content/uploads/elementor/css/post-1277cd77cd.css?ver=1630930519' media='all' />
-      <link rel='stylesheet' id='google-fonts-1-css'  href='https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;display=auto&amp;ver=5.8.1' media='all' />
-      <link rel='stylesheet' id='elementor-icons-shared-0-css'  href='wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min52d552d5.css?ver=5.15.3' media='all' />
-      <link rel='stylesheet' id='elementor-icons-fa-solid-css'  href='wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min52d552d5.css?ver=5.15.3' media='all' />
-      <link rel='stylesheet' href='css/style.css' media='all' />
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-223635208-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
 
-  gtag('config', 'UA-223635208-1');
-</script>
-   </head>
-   <body style="background: url:launcher-background-windows-scaled.jpg" class="page-template page-template-elementor_canvas page page-id-12 ast-single-post ast-inherit-site-logo-transparent ast-hfb-header ast-desktop ast-separate-container ast-right-sidebar astra-3.6.8 ast-normal-title-enabled elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-12">
-      <div data-elementor-type="wp-page" data-elementor-id="12" class="elementor elementor-12" data-elementor-settings="[]">
-         <div class="elementor-section-wrap">
-            <section style="background-image: url('image/launcher-background-windows-scaled.jpg');" class="elementor-section elementor-top-section elementor-element elementor-element-b3f3262 elementor-section-full_width elementor-section-height-full elementor-section-height-default elementor-section-items-middle" data-id="b3f3262" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-               <div class="elementor-container elementor-column-gap-no">
-                  <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-b47c886" data-id="b47c886" data-element_type="column">
-                     <div class="elementor-widget-wrap elementor-element-populated">
-                        <div class="elementor-element elementor-element-bbc2819 elementor-widget elementor-widget-heading" data-id="bbc2819" data-element_type="widget" data-widget_type="heading.default">
-                           <div class="elementor-widget-container">
-                              <span class="elementor-heading-title elementor-size-default"><span style="font-size:90px;">Cricut<span></span>		
-                           </div>
-                        </div>
-                        <section class="elementor-section elementor-inner-section elementor-element elementor-element-c5f998f elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="c5f998f" data-element_type="section">
-                           <div class="elementor-container elementor-column-gap-default">
-                              <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-13fab72" data-id="13fab72" data-element_type="column">
-                                 <div class="elementor-widget-wrap">
-                                 </div>
-                              </div>
-                              <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-034451a" data-id="034451a" data-element_type="column">
-                                 <div class="elementor-widget-wrap elementor-element-populated">
-                                    <div class="elementor-element elementor-element-6a654b2 elementor-widget elementor-widget-heading" data-id="6a654b2" data-element_type="widget" data-widget_type="heading.default">
-                                       <div class="elementor-widget-container">
-                                          <h3 class="elementor-heading-title elementor-size-default">Design Space &#xa9; or Windows &#xa9;<h3>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </section>
-                        <section class="elementor-section elementor-inner-section elementor-element elementor-element-1fcbe57 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="1fcbe57" data-element_type="section">
-                           <div class="elementor-container elementor-column-gap-default">
-                              <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-4278852" data-id="4278852" data-element_type="column">
-                                 <div class="elementor-widget-wrap">
-                                 </div>
-                              </div>
-                              <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-10b08c9" data-id="10b08c9" data-element_type="column">
-                                 <div class="elementor-widget-wrap elementor-element-populated">
-                                    <div class="elementor-element elementor-element-59d5d21 elementor-widget elementor-widget-text-editor" data-id="59d5d21" data-element_type="widget" data-widget_type="text-editor.default">
-                                       <div class="elementor-widget-container">
-                                          <p style="text-align: left; font-size:17.5px">Set up a new Cricut product, browse projects, start designing, and more.</p>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </section>
-                        <section class="elementor-section elementor-inner-section elementor-element elementor-element-88c06bc elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="88c06bc" data-element_type="section">
-                           <div class="elementor-container elementor-column-gap-default">
-                              <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-b762938" data-id="b762938" data-element_type="column">
-                                 <div class="elementor-widget-wrap">
-                                 </div>
-                              </div>
-                              <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-e97dc9a" data-id="e97dc9a" data-element_type="column">
-                                 <div class="elementor-widget-wrap elementor-element-populated">
-                                    <div class="elementor-element elementor-element-0686d97 elementor-widget elementor-widget-html" data-id="0686d97" data-element_type="widget">
-                                       <div class="elementor-widget-container">
-                                          <a class="reset btn-gradient-1" href="create-id.php">Register( New Users)</a>
-                                          
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                              <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-ab6146b" data-id="ab6146b" data-element_type="column">
-                                 <div class="elementor-widget-wrap elementor-element-populated">
-                                    <div class="elementor-element elementor-element-d8adc21 elementor-widget elementor-widget-html" data-id="d8adc21" data-element_type="widget">
-                                       <div class="elementor-widget-container">
-                                          <a class="reset btn-gradient-1" href="login.php">Sign In</a>
-                                          
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </section>
-                        <section class="elementor-section elementor-inner-section elementor-element elementor-element-1ed8d01 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="1ed8d01" data-element_type="section">
-                           <div class="elementor-container elementor-column-gap-default">
-                              <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-51938f6" data-id="51938f6" data-element_type="column">
-                                 <div class="elementor-widget-wrap">
-                                 </div>
-                              </div>
-                              <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-5703c43" data-id="5703c43" data-element_type="column">
-                                 <div class="elementor-widget-wrap elementor-element-populated">
-                                    <div class="elementor-element elementor-element-4f27f1c elementor-view-default elementor-position-top elementor-vertical-align-top elementor-widget elementor-widget-icon-box" data-id="4f27f1c" data-element_type="widget" data-widget_type="icon-box.default">
-                                       <div class="elementor-widget-container">
-                                          <div class="elementor-icon-box-wrapper">
-                                             <div class="elementor-icon-box-content">
-                                                <h3 class="elementor-icon-box-title">
-                                                   <span  >
-                                                   </span>
-                                                </h3>
-                                                <p class="elementor-icon-box-description">
-                                                   .					
-                                                </p>
-                                             </div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </section>
-                     </div>
-                  </div>
-               </div>
-            </section>
-         </div>
-      </div>
+<!-- Begin Jekyll SEO tag v2.8.0 -->
+<title>GitHub - Ij.start.canon || Ij Canon Printer Setup || https://ij.start.canon</title>
+<meta name="generator" content="Jekyll v3.9.2" />
+<meta property="og:title" content="IJ.Start.Canon Setup | Canon IJ Printer Setup" />
+<meta property="og:locale" content="en_US" />
+<meta name="description" content="IJ Start Canon setup process &amp; Installation Guide for Canon printer. Ij.start.cannon setup is that the best way for the ij.start.canon to setup wireless setup canon printer ." />
+<meta property="og:description" content="IJ Start Canon setup process &amp; Installation Guide for Canon printer. Ij.start.cannon setup is that the best way for the ij.start.canon to setup wireless connection." />
+<link rel="canonical" href="https://github.com/ijstartcanonsetip/ijstartcanon.github.io/" />
+<meta property="og:url" content="https://github.com/ijstartcanonsetip/ijstartcanon.github.io" />
+<meta property="og:site_name" content="Install IJ Canon Printer - Ij.Start.Canon - Canon IJ Printer Driver" />
+<meta property="og:type" content="website" />
+<meta name="twitter:card" content="summary" />
+<meta property="twitter:title" content="IJ.Start.Canon Setup || Canon IJ Printer Setup || ij.start.canon" />
+<meta name="msvalidate.01" content="" />
+    <link rel="stylesheet"  href="style.css">
+    <!-- start custom head snippets, customize with your own _includes/head-custom.html file -->
+
+<!-- Setup Google Analytics -->
+
+
+
+<!-- You can set your favicon here -->
+<!-- link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" -->
+
+<!-- end custom head snippets -->
+
+  </head>
+  <body>
+    <a id="skip-to-content" href="#content">Skip to the content.</a>
+
+    <header class="page-header" role="banner">
+      <h1 class="project-name">IJ.Start.Canon Setup   Canon IJ Printer Setup</h1>
+      <h2 class="project-tagline">IJ Start Canon setup process & Installation Guide for Canon printer. Ij.start.cannon setup is that the best way for the ij.start.canon to setup wireless connection.</h2>
       
-    
-      <script src='wp-content/themes/astra/assets/js/minified/frontend.mina4f3a4f3.js?ver=3.6.8' id='astra-theme-js-js'></script>
-      <script src='wp-includes/js/wp-embed.minf658f658.js?ver=5.8.1' id='wp-embed-js'></script>
-      <script src='wp-content/plugins/elementor/assets/js/webpack.runtime.minccfbccfb.js?ver=3.4.2' id='elementor-webpack-runtime-js'></script>
-      <script src='wp-includes/js/jquery/jquery.minaf6caf6c.js?ver=3.6.0' id='jquery-core-js'></script>
-      <script src='wp-includes/js/jquery/jquery-migrate.mind617d617.js?ver=3.3.2' id='jquery-migrate-js'></script>
-      <script src='wp-content/plugins/elementor/assets/js/frontend-modules.minccfbccfb.js?ver=3.4.2' id='elementor-frontend-modules-js'></script>
-      <script src='wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min05da05da.js?ver=4.0.2' id='elementor-waypoints-js'></script>
-      <script src='wp-includes/js/jquery/ui/core.min35d035d0.js?ver=1.12.1' id='jquery-ui-core-js'></script>
-      <script src='wp-content/plugins/elementor/assets/lib/swiper/swiper.min48f548f5.js?ver=5.3.6' id='swiper-js'></script>
-      <script src='wp-content/plugins/elementor/assets/lib/share-link/share-link.minccfbccfb.js?ver=3.4.2' id='share-link-js'></script>
-      <script src='wp-content/plugins/elementor/assets/lib/dialog/dialog.mina288a288.js?ver=4.8.1' id='elementor-dialog-js'></script>
-      <script src='wp-content/plugins/elementor/assets/js/frontend.minccfbccfb.js?ver=3.4.2' id='elementor-frontend-js'></script>
-      <script src='wp-content/plugins/elementor/assets/js/preloaded-modules.minccfbccfb.js?ver=3.4.2' id='preloaded-modules-js'></script>
-   </body>
+      
+    </header>
+
+    <main id="content" class="main-content" role="main">
+      <h1 id="ijstartcanon-setup--canon-ij-printer-setup">IJ.Start.Canon Setup || Canon IJ Printer Setup</h1>
+
+<p><a href="https://ij.startcani.com/setup/web_manual/"><img src="Get-Started.png" alt="canon.com/ijsetup" /></a></p>
+
+<p><a href="https://github.com/ijstartcanonsetip/ijstartcanon.github.io">Ij Start Canon</a> is the offical Web address Provided By Canon So You can Download Driver, Manual &amp; Guides for your Canon Printer. Consider it a One-Stop Shop For All Your Printer Needs. You Can manually find the Correct Version of Driver for Your Printer. Just Type the Printer Model Number &amp; it Will Automatically Find the relevant Driver For Your Printer.</p>
+
+<h2 id="purchasing-the-canon-printer">Purchasing the Canon printer</h2>
+<ol>
+  <li>There are two ways of purchasing the canon printer.</li>
+</ol>
+
+<p>2.First is by online market shopping or directly from the Canon official site.</p>
+
+<p>3.This first of all you must go to the online shopping application.</p>
+
+<p>4.Then type the name as well as the model number of the printer you need.</p>
+
+<p>5.Click on add to cart.</p>
+
+<p>6.Then that simply click on the purchase option.</p>
+
+<p>7.Now make payment using any suitable payment method.</p>
+
+<p>8.The product will be delivered to your desired location.</p>
+
+<p>9.The same is with the official canon site.</p>
+
+<p>10.You can also buy it right from the offline canon store.</p>
+
+<h2 id="downloading-the-ijstartcanon-printer-setup">Downloading the ij.start.canon printer setup:</h2>
+<p>Now downloading of the printer setup takes speed depending on your device as well as internet speed. The procedure is very simple;</p>
+
+<p>*Just turn on your pc or any other device.</p>
+
+<p>*Go to the web browser of your pc or device and enter the URL <a href="https://github.com/ijstartcanonsetip/ijstartcanon.github.io">Ij Start Canon</a>.</p>
+
+<p>*Then you will be taken to the official website of the ij.start.canon.</p>
+
+<p>*Now click on the ij start canon setup option present there in red color format.</p>
+
+<p>*After that select the model number of your printer using the 3 types of selecting tools.</p>
+
+<p>*Then after this just proceed and the drivers, as well as essential software available for this printer, will display in your front.</p>
+
+<p>*This will show a download button.</p>
+
+<p>*Just press the mouse pointer on the download button.</p>
+
+<p>*A popup window will appear.</p>
+
+<p>*Select the operating system of your device or sometimes it automatically detects the operating system.</p>
+
+<p>*Then click on I Agree and then on the download button.</p>
+
+<p>*Now the downloading will start.</p>
+
+<p>*This will take some time depending on the speed of your internet as well as the operating system.</p>
+
+
+      <footer class="site-footer">
+        
+        <span class="site-footer-credits">This page was generated by <a href="https://pages.github.com">GitHub Pages</a>.</span>
+      </footer>
+    </main>
+  </body>
 </html>
